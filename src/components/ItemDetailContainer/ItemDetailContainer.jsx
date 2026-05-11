89e16b86a6618cc884ce3a9c77a449d70a3c4242
fmonsari <fmonsari@gmail.com>
2026-05-11 20:10:47 -0300

    Imports faltantes

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { ItemDetail } from "../ItemDetail/ItemDetail";
 
 export const ItemDetailContainer = () => {
 const { id } = useParams();
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ItemList } from "../ItemList/ItemList";
 
 export const ItemListContainer = () => {
     const [products, setProducts] = useState ([]);
     const [loading, setLoading] = useState (true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
          fetch("/data/products.json")
             .then((res) => res.json())
             .then((data) => setProducts(data))
-            .catch((err) => console.log("Hubo un error:", err))
+            .catch((err) => {
+              console.log("Hubo un error:", err)
+            setError(true);
+            })
             .finally(() => {
                 setLoading(false);
     });
   }, []);
 
   if (loading) return <p>Cargando...</p>;
-  if (error) return <p>Error</p>;
+  if (error) return <p>Error al cargar los productos</p>;
 
   return (
   <section>
